dafa9e9a7368542fe124509f9fcfb1eb73d3d4c3
oscarfava22 <32568957+oscarfava22@users.noreply.github.com>
2018-02-15 23:23:32 +0100

    loginClass added.

---
+++ app/src/main/java/com/example/oscar/laberinto/Login.java
@@ -0,0 +1,30 @@
+package com.example.oscar.laberinto;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+
+
+public class Login extends AppCompatActivity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClickButtonLogin (View view){
+
+        AutoCompleteTextView editText = (AutoCompleteTextView) findViewById(R.id.user);
+        //buscar si el teniem a la base de dades
+        menu.usuari.setNom(editText.getText().toString());
+        Intent intent = new Intent(this, menu.class);
+        startActivity(intent);
+
+    }
+}
+
